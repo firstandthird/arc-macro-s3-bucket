b1c12a22da9f3adcd323f791b91f9b2dc2d3435c
orthagonal <orthagonal1@gmail.com>
2020-02-21 18:16:59 +0000

    first version

--- index.js
+++ index.js
@@ -5,46 +5,53 @@ const AWS = require('aws-sdk');
  * @param {object} cloudformation - the current AWS::Serverless CloudFormation template
  * @param {object} stage - the application stage (one of `staging` or `production`)
  */
-module.exports = function async arcBucketMacro(arc, cloudformation, stage) {
+module.exports = async function arcBucketMacro(arc, cloudformation, stage) {
   const config = arc.s3;
   const buckets = {};
   const aws = new AWS.S3();
   // for each bucket:
-  config.forEach(b => {
-    let bucketName = b[0];
-    if (b.length === 1) {
-      // bucketname will be "{name}-production" or "{name}-staging"
-      // if bucket is marked 'unique' it will have a timestamp appended to the end:
-      bucketName = `${b[1]}-${b[0]}-${b[2] && b[2].toLowerCase() === 'unique' ? new Date().getTime() : ''}`;
-    }
-    // see if buckets exist on s3:
-    let exists = false;
-    const params = {
-      Bucket: bucketName,
-    };
-    try {
-      await aws.headBucket({ Bucket: bucketName }).promise();
-      exists = true;
-    } catch (e) {
-    }
-    // create buckets if it doesn't exist:
-    await aws.createBucket({
-      Bucket: bucketName
-      /*
-      // any config options here?
-      */
-    });
-    // modify arcformation
-    cloudformation.Resources.Role.Properties.Policies.push({
-      PolicyName: 'ArcS3Access',
-      PolicyDocument: {
-        Statement: [{
-          Effect: 'Allow',
-          Action: 's3:*',
-          Resource: '*'
-        }]
+  await Promise.all(config.map(b => {
+    return new Promise(async (resolve) => {
+      let bucketName = b[0];
+      if (b.length > 1) {
+        // abort if bucket does not match requested stage:
+        if (b[0] !== stage) {
+          return resolve();
+        }
+        // bucketname will be "{name}-production" or "{name}-staging"
+        // if bucket is marked 'unique' it will have a timestamp appended to the end:
+        bucketName = `${b[1]}-${b[0]}${b[2] && b[2].toLowerCase() === 'unique' ? new Date().getTime() : ''}`;
       }
+      // see if buckets exist on s3:
+      let exists = false;
+      const params = {
+        Bucket: bucketName,
+      };
+      try {
+        const res = await aws.headBucket({ Bucket: bucketName }).promise();
+        exists = true;
+      } catch (e) {
+        // throws error if bucket does not exist
+      }
+      // create buckets if it doesn't exist:
+      if (!exists) {
+        const res = await aws.createBucket({
+          Bucket: bucketName
+        }).promise();
+      }
+      // modify arcformation
+      cloudformation.Resources.Role.Properties.Policies.push({
+        PolicyName: 'ArcS3Access',
+        PolicyDocument: {
+          Statement: [{
+            Effect: 'Allow',
+            Action: 's3:*',
+            Resource: '*'
+          }]
+        }
+      });
+      return resolve();
     });
-  });
+  }));
   return cloudformation
 }
---
+++ test.js
@@ -0,0 +1,37 @@
+// a simple test, you can manually verify
+const tap = require('tap');
+const arcBucketMacro = require('./index.js');
+const AWS = require('aws-sdk');
+
+tap.test('load cloudformation', async t => {
+  const arc = { s3: [
+    ['staging', 'no-real-bucketeh'],
+    ['staging', 'no-real-bucketeh', 'unique'],
+    ['production', 'should-not-exist', 'unique'],
+  ]};
+  const cloudformation = {
+    Resources: {
+      Role: {
+        Properties: {
+          Policies: []
+        }
+      }
+    }
+  };
+  const result = await arcBucketMacro(arc, cloudformation, 'staging')
+  t.equal(result.Resources.Role.Properties.Policies.length, 2);
+  const aws = new AWS.S3();
+  try {
+    await aws.headBucket({ Bucket: 'no-real-bucketeh-staging' }).promise();
+  } catch (e) {
+    console.log(e);
+    t.fail();
+  }
+  try {
+    const res = await aws.headBucket({ Bucket: 'should-not-exist' }).promise();
+    t.fail();
+  } catch (e) {
+    // error is supposed to occur here
+  }
+  t.end();
+});
