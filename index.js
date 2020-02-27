b0e5b3f7ae1afd0401a68069264bb5372cf92d4e
orthagonal <orthagonal1@gmail.com>
2020-02-27 18:52:23 +0000

    create bucket with template

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ const caps = require('lodash.capitalize');
 module.exports = async function arcBucketMacro(arc, cloudformation, stage) {
   const config = arc.s3;
   const buckets = {};
-  // get the paths with the correct name formatting:
+  // set the S3_BUCKET env variable for every lambda we create
+  // first get the correct path names with the correct formatting:
   const paths = arc.http.map(list => {
     return list.map(component => {
       const p = component.replace(/\//g, '');
@@ -17,19 +18,20 @@ module.exports = async function arcBucketMacro(arc, cloudformation, stage) {
       return pList.map(caps).join('')
     }).join('');
   });
-  let mainBucket;
+  let s3Bucket;
   arc.aws.forEach(f => {
     if (f[0] === 'bucket') {
-      mainBucket = f[1];
+      s3Bucket = f[1];
     }
   });
-  mainBucket = process.env.S3_BUCKET || mainBucket;
+  s3Bucket = process.env.S3_BUCKET || s3Bucket;
+  // loop over every path and set the ENV var:
   paths.map(p => {
     if (p === 'Get') {
       p = 'GetIndex';
     }
     if (cloudformation.Resources[p]) {
-      cloudformation.Resources[p].Properties.Environment.Variables.S3_BUCKET = mainBucket;
+      cloudformation.Resources[p].Properties.Environment.Variables.S3_BUCKET = s3Bucket;
     }
   })
   const aws = new AWS.S3();
@@ -57,11 +59,16 @@ module.exports = async function arcBucketMacro(arc, cloudformation, stage) {
       } catch (e) {
         // throws error if bucket does not exist
       }
-      // create buckets if it doesn't exist:
+      // create bucket in the cloudformation template if it doesn't exist:
       if (!exists) {
-        const res = await aws.createBucket({
-          Bucket: bucketName
-        }).promise();
+        const s3Resource = {
+          Type: "AWS::S3::Bucket",
+          Properties: {
+            BucketName: bucketName,
+            // todo: any other properties we want?
+          }
+        };
+        cloudformation.Resources[bucketName] = s3Resource;
       }
       const env = process.env.S3_BUCKET || '';
       // modify arcformation
