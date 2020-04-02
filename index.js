91c04474e14a81ec4803e5100cdd71a63faab33c
orthagonal <orthagonal1@gmail.com>
2020-04-02 03:30:38 +0000

    cors

--- index.js
+++ index.js
@@ -11,14 +11,34 @@ module.exports = function arcBucketMacro(arc, cloudformation, stage) {
   cloudformation.Resources.ArcS3Bucket = {
     Type: 'AWS::S3::Bucket'
   };
-  if (arc.s3[0] === 'versioning') {
+  if (arc.s3.includes('versioning')) {
     cloudformation.Resources.ArcS3Bucket.Properties = {
       VersioningConfiguration: {
         Status: 'Enabled'
       }
     };
   }
-
+  if (arc.s3.includes('cors')) {
+    if (cloudformation.Resources.ArcS3Bucket.Properties) {
+      cloudformation.Resources.ArcS3Bucket.Properties.CorsConfiguration = {
+        CorsRules: [{
+          AllowedOrigins: ['*'],
+          AllowedHeaders: ['*'],
+          AllowedMethods: ['GET', 'POST', 'HEAD'],
+        }]
+      };
+    } else {
+      cloudformation.Resources.ArcS3Bucket.Properties = {
+        CorsConfiguration: {
+          CorsRules: [{
+            AllowedOrigins: ['*'],
+            AllowedHeaders: ['*'],
+            AllowedMethods: ['GET', 'POST', 'HEAD'],
+          }]
+        }
+      };
+    }
+  }
   Object.entries(cloudformation.Resources).forEach(([key, value]) => {
     if (value.Type && value.Type === 'AWS::Serverless::Function') {
       cloudformation.Resources[key].Properties.Environment.Variables.S3_BUCKET = { Ref: 'ArcS3Bucket' };
